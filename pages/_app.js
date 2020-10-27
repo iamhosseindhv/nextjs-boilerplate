4b18f1d929b63f67f5de39e333cf68c9da99a2de
Hossein Dehnokhalaji <hossein.dehnavi98@yahoo.com>
2020-10-27 00:08:31 +0000

    Add language to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { createContext, Fragment } from 'react';
 import App from 'next/app';
 import Head from 'next/head';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/utils/theme';
+import theme, { getMuiTheme } from '../src/utils/theme';
 import Layout from '../src/components/Layout';
+import { LANGUAGES } from 'server/util';
+
+const LangContext = createContext();
 
 class MyApp extends App {
+    constructor(props) {
+        super(props);
+        const userLanguage = props.pageProps.userLanguage || LANGUAGES.EN;
+        this.state = {
+            theme: getMuiTheme(userLanguage),
+            context: {
+                lang: userLanguage,
+            },
+        };
+    }
+
     static getInitialProps = async ({ Component, ctx }) => {
         let pageProps = {};
         if (Component.getInitialProps) {
             pageProps = await Component.getInitialProps(ctx);
+        }
+
+        if (!process.browser) {
+            pageProps.userLanguage = ctx.query.userLanguage;
         }
 
         return { pageProps };
@@ -34,21 +52,23 @@ class MyApp extends App {
                     <meta name="viewport" content="user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height" />
                     <link rel="stylesheet" type="text/css" href="/routes.css" />
                 </Head>
-                <Layout>
-                    <TransitionGroup>
-                        <CSSTransition
-                            key={this.props.router.route}
-                            classNames="fadeTranslate"
-                            // classNames="fade"
-                            timeout={1000}
-                        >
-                            <MuiThemeProvider theme={theme}>
-                                <CssBaseline />
-                                <Component {...pageProps} />
-                            </MuiThemeProvider>
-                        </CSSTransition>
-                    </TransitionGroup>
-                </Layout>
+                <LangContext.Provider value={this.state.context}>
+                    <Layout>
+                        <TransitionGroup>
+                            <CSSTransition
+                                key={this.props.router.route}
+                                classNames="fadeTranslate"
+                                // classNames="fade"
+                                timeout={1000}
+                            >
+                                <MuiThemeProvider theme={theme}>
+                                    <CssBaseline />
+                                    <Component {...pageProps} />
+                                </MuiThemeProvider>
+                            </CSSTransition>
+                        </TransitionGroup>
+                    </Layout>
+                </LangContext.Provider>
             </Fragment>
         );
     }
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,8 +1,9 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeOptions } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
+import { LANGUAGES } from '../../server/util';
 
-const theme = createMuiTheme({
+const defaultTheme: ThemeOptions = {
     palette: {
         primary: {
             light: purple[300],
@@ -15,6 +16,13 @@ const theme = createMuiTheme({
             dark: green[700],
         },
     },
+};
+
+const theme = createMuiTheme(defaultTheme);
+
+export const getMuiTheme = (newLang = LANGUAGES.EN) => createMuiTheme({
+    ...defaultTheme,
+    direction: newLang === LANGUAGES.EN ? 'ltr' : 'rtl',
 });
 
 export default theme;
